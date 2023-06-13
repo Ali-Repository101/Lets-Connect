6699fa930abee797f1471447def276c47b4767b5
dev7417 <devgangwar029@gmail.com>
2023-06-13 11:25:38 +0530

    adding loader and lazy loading

---
+++ src/components/Loader.js
@@ -0,0 +1,21 @@
+import '../components/Loader.scss'
+const Loader = () => {
+    return (
+        <>
+    <div>
+    <div class="mesh-loader">
+      <div class="set-one">
+        <div class="circle"></div>
+        <div class="circle"></div>
+      </div>
+      <div class="set-two">
+        <div class="circle"></div>
+        <div class="circle"></div>
+      </div>
+    </div>
+  </div>
+
+        </>
+    )
+}
+export default Loader
--- src/components/users/UserRoutes.tsx
+++ src/components/users/UserRoutes.tsx
@@ -1,15 +1,31 @@
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom"
-import Login from "./Login"
-import SignUp from "./SignUp"
+import { createBrowserRouter, RouterProvider, Outlet, } from "react-router-dom"
+import { Suspense, lazy, useState, useEffect } from "react"
+import Loader from "../Loader"
+const Login = lazy(() => import("./Login"))
+const SignUp = lazy(() => import("./SignUp"))
 const UserRoutes = () => {
+    const [loading, setLoading] = useState(true)
+    console.log("loading-------------", loading)
+    useEffect(() =>{
+        setTimeout(loader, 3000)
+    }, [])
+    const loader = () => {
+        setLoading(false)
+    }
+    if (loading) {
+        return <h1><Loader/></h1>
+    } else {
+        <SignUp/>
+    }
     const router = createBrowserRouter([{
         path: 'login',
-        element: <Login />
+        element: <Suspense fallback={<><p>Loading.....</p></>}><Login /></Suspense>
     }, {
         path: '/',
-        element: <SignUp />
+        element: <Suspense fallback={<><p>Loading.......</p></>}><SignUp /></Suspense>
     }
     ])
+
     return (
         <>
             <RouterProvider router={router} />
